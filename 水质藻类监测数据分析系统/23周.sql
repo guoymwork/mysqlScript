b99ed25204783a07677cb8bd00dd9457d8becb53
guoymwork <guoymwork@163.com>
2018-06-06 10:44:12 +0800

    add

--- 水质藻类监测数据分析系统/23周.sql
+++ 水质藻类监测数据分析系统/23周.sql
@@ -29,6 +29,35 @@ DELETE
 FROM Work_Data_Hydrology 
 where LakeCode='104'
 /**********************************************************************************/
+--导入大屯水库 6-6水文数据
+insert Work_Data_Hydrology
+(
+	   [Id]
+      ,[MonitorDate]
+      ,[LakeCode]
+      ,[SectionCode]
+      ,[WindDirection]
+      ,[WindPower]
+      ,[WaterDepth]
+      ,[WaterLevel]
+)
+SELECT newid() as id
+      ,[采样日期]
+      ,lake.LakeCode
+	  ,sec.SectionCode  
+      ,[风向]
+      ,[风力]
+      ,[水深]
+      ,[水位]
+FROM [dbo].[大屯水库-水文6-6] imp
+left join Work_Algae_LakeLibrary lake on LakeName=imp.[湖泊]
+left join Work_Algea_Section sec on sec.SectionName=imp.[站名]
+
+--水库编号
+SELECT *
+FROM Work_Algae_LakeLibrary
+where 	LakeName='大屯水库'
+
 
 
 
